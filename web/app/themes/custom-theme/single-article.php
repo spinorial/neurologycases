95503f90d51c1b989cc035b053c889bcc530f339
Joseph Marshall <josephmarshall@Josephs-MacBook.local>
2017-11-05 23:50:43 +0000

    some formatting

--- web/app/themes/custom-theme/single-article.php
+++ web/app/themes/custom-theme/single-article.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 			 get_template_part( 'article/content', get_post_type() );
 
-			the_post_navigation();
+			//	the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
